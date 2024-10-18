57566dc3549b5196d66c2b5bca80b4e49be20cec
calteen <pjay32547@gmail.com>
2024-10-18 10:26:44 +0530

    some basic

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "The MBSA Server is Running"
-
-@app.route("/mbsa")
-def mbsa():
     return render_template('index.html')
+
+@app.route("/product")
+def mbsa():
+    return render_template('product.html')
 
