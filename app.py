33b9f1040c39d612227f05f8dccf26c22a8b5706
calteen <pjay32547@gmail.com>
2024-10-18 10:28:43 +0530

    jinja try

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return render_template('index.html')
+    return render_template('index.html',title="Build Anything with Themebook", name="Jay")
 
 @app.route("/product")
 def mbsa():
